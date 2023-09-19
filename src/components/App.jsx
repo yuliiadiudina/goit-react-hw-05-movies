aa1748ed58edcef1b85bbb9ba9a9fd58c7a8624b
Yuliia Diudina <yuliiadiudina@gmail.com>
2023-09-19 16:03:28 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,8 +14,8 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetails />}>
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<MovieCast />} />
           <Route path="reviews" element={<MovieReviews />} />
         </Route>
